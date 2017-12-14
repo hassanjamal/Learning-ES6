c1be87468ab7dea60cdd9a96d6d8fa66535db6b4
HassanJamal <hs.jamal@gmail.com>
2017-12-15 02:15:57 +0530

    Finishing with let var and const

---
+++ src/const.js
@@ -0,0 +1,8 @@
+const names = ['hassan', 'jamal'];
+
+// names = ['newName', 'newLastName'] // this will throw error
+
+// however
+names.push('Rav');
+
+console.log(names);
---
+++ src/let.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function fire(bool) {
+  if (bool) {
+    let foo = 'bar';
+    console.log(foo);
+  } else {
+    console.log(foo);
+  }
+}
+
+fire(false);
+fire(true);
